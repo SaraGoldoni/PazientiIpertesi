f03c18ce444c2cfea4f803a48962ba7991736fb5
SaraGoldoni <sara.goldoni@hotmail.it>
2023-05-22 15:41:28 +0200

    update Paziente.fxml e PazienteController
    Update di paziente controller e paziente fxml

--- src/main/java/progettoingegneria/pazientiipertesi/PazienteController.java
+++ src/main/java/progettoingegneria/pazientiipertesi/PazienteController.java
@@ -1,4 +1,31 @@
 package progettoingegneria.pazientiipertesi;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class PazienteController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+
+    public void SwitchToMemo(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MemoGiornaliere.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
