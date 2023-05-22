fd889ad2346a50fd20d1f34f1a4dc2fcf3a8aa36
SaraGoldoni <sara.goldoni@hotmail.it>
2023-05-22 16:32:44 +0200

    Prova_non_definitivo
    Inizio prova per vedere come fare a prendere il testo inserito e metterlo nel database

--- src/main/java/progettoingegneria/pazientiipertesi/PazienteController.java
+++ src/main/java/progettoingegneria/pazientiipertesi/PazienteController.java
@@ -12,13 +12,12 @@ import javafx.stage.Stage;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.sql.*;
 
 public class PazienteController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-
-
 
 
     public void SwitchToMemo(javafx.event.ActionEvent event) throws IOException {
@@ -27,5 +26,30 @@ public class PazienteController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    private TextField SBP;
+    @FXML
+    private TextField DBP;
+
+    public void InsertIntoMemo(ActionEvent event) throws SQLException {
+        DatabaseConnection conn = new DatabaseConnection();
+        Connection c = conn.link();
+        try (Statement st = c.createStatement()) {
+            ResultSet rs = null;
+            PreparedStatement pstmt= null;
+            String query = ("INSERT INTO Memo(SBP, DBP, sintomi, farmaco, quantità, data, ora)\" + \"VALUES (?, ?, ?, ?,?,?,?");
+            pstmt = c.prepareCall(query);
+            String pressioneMassima = SBP.getText();
+            String pressioneMinima = DBP.getText();
+            pstmt.setString(1, pressioneMassima);
+            pstmt.setString(2, pressioneMinima);
+            pstmt.setString(3, ); // consider setInt() might be more appropriate
+            pstmt.setString(4,);
+            pstmt.setString(5,);
+            pstmt.setString(6,);
+            pstmt.setString(7,);
+
+            int status = pstmt.executeUpdate();
     }
 }
