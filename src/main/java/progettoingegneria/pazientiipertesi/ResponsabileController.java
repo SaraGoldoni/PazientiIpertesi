1f996d7a26fb7d7437a68d38a1a4104cf321242f
SaraGoldoni <sara.goldoni@hotmail.it>
2023-06-12 18:02:38 +0200

    Responsabile
    Responsabile inserimento paziente e medico funzionante

--- src/main/java/progettoingegneria/pazientiipertesi/ResponsabileController.java
+++ src/main/java/progettoingegneria/pazientiipertesi/ResponsabileController.java
@@ -24,6 +24,24 @@ private TextField nome_paz;
     @FXML
     private DatePicker data_nascita_paz;
 
+    @FXML
+    private TextField cf_medico;
+    @FXML
+    private TextField nome_med;
+    @FXML
+    private TextField cognome_med;
+    @FXML
+    private TextField mail;
+    @FXML
+    private TextField nome_utente_med;
+    @FXML
+    private TextField pass_med;
+    @FXML
+    private DatePicker data_nascita_med;
+    public void turnBack(ActionEvent event) throws IOException {
+        Controller.Switch("Responsabile.fxml", event);
+    }
+
     public void NewPaziente(ActionEvent event) throws IOException {
         Controller.Switch("ResponsabilePaziente.fxml",event);
     }
@@ -64,16 +82,40 @@ private TextField nome_paz;
         ps2.executeUpdate();
         c.commit();
 
-
-
-
-
-
-
-        Controller.Switch("Responsabile.fxml",event);
+        Controller.Switch("ResponsabilePaziente.fxml",event);
     }
-    public void InsertMedico(ActionEvent event) throws IOException {
-        Controller.Switch("Responsabile.fxml",event);
+    public void InsertMedico(ActionEvent event) throws IOException, SQLException {
+        DatabaseConnection conn = new DatabaseConnection();
+        Connection c = conn.link();
+        String query1 = ("INSERT INTO pazientiipertesi.Medico(codicefiscale, nome, cognome, data_nascita, email) VALUES (?, ?, ?, ?, ?)");
+        String query3= ("INSERT INTO pazientiipertesi.Login(nomeutente, password, tipo, paziente, medico) VALUES (?, ?, ?, ?, ?)");
+        String CF_med, N, C, D_N, email, username_med, pass_medico;
+        CF_med=cf_medico.getText();
+        N= nome_med.getText();
+        C = cognome_med.getText();
+        D_N= data_nascita_med.getValue().toString();
+        email= mail.getText();
+        username_med= nome_utente_med.getText();
+        pass_medico= pass_med.getText();
+        PreparedStatement ps1= c.prepareStatement(query1);
+        PreparedStatement ps3= c.prepareStatement(query3);
+        c.setAutoCommit(false);
+        ps1.setString(1, CF_med);
+        ps1.setString(2, N);
+        ps1.setString(3, C);
+        ps1.setDate(4, Date.valueOf(D_N));
+        ps1.setString(5,email);
+
+        ps3.setString(1,username_med);
+        ps3.setString(2,pass_medico);
+        ps3.setInt(3, 0);
+        ps3.setNull(4, Types.NULL);
+        ps3.setString(5, CF_med);
+
+        ps1.executeUpdate();
+        ps3.executeUpdate();
+        c.commit();
+        Controller.Switch("ResponsabileMedico.fxml",event);
     }
 
 }
